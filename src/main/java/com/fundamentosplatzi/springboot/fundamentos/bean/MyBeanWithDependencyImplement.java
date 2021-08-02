98dbb772fe7cf9ab3541407446197462e97101d0
David Almeida <djalmeida3@gmail.com>
2021-08-01 21:57:46 -0500

    Uso de logs

--- src/main/java/com/fundamentosplatzi/springboot/fundamentos/bean/MyBeanWithDependencyImplement.java
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/bean/MyBeanWithDependencyImplement.java
@@ -1,5 +1,8 @@
 package com.fundamentosplatzi.springboot.fundamentos.bean;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class MyBeanWithDependencyImplement implements MyBeanWithDependency {
 
     private MyOperation myOperation;
@@ -10,7 +13,9 @@ public class MyBeanWithDependencyImplement implements MyBeanWithDependency {
 
     @Override
     public void printWithDependency() {
+        log.info("Hemos ingresado al metodo printWithDependency");
         int numero=1;
+        log.debug("El numero enviado como parametro a la dependencia operacion es: " + numero);
         System.out.println(myOperation.sum(numero));
         System.out.println("Hola desde la implementacion de un bean con dependencia");
     }
